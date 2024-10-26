59000cca6117832f3d051a90aff883091bd8823c
Ryan Miller <aerilym@pm.me>
2024-10-27 08:39:07 +1100

    chore: add eth address command info to join and onboarding messages

--- bot/messages/onboarding.ts
+++ bot/messages/onboarding.ts
@@ -3,6 +3,7 @@ import { supportMessage } from './support.ts';
 import { getBasicStopInfoMessage } from './util.ts';
 import { getJoinCommandConsent } from './join.ts';
 import { BOT_USERNAME } from '../../env.ts';
+import { addressCommand } from "../commands/address.ts";
 
 export const getOnboardingMessage =
   () => `Welcome to the ${BOT_USERNAME}! This bot helps you monitor the Session Nodes you stake to.
@@ -13,7 +14,11 @@ To start using the bot, you must first opt in by using the /${joinCommand.name} 
 
 ${getJoinCommandConsent()}
 
-You can also send your Ethereum address to the bot as part of the /${joinCommand.name} command. This will allow to bot to find your staked Session nodes for you, and allow you to easily add them to your watchlist:
+After you join, you can use the /${addressCommand.name} command to add and manage Ethereum addresses. This will allow to bot to find your staked Session nodes for you, and allow you to easily add them to your watchlist:
+
+/${addressCommand.name} add <ethereum-address>
+
+You can also send your Ethereum address to the bot as part of the /${joinCommand.name} command:
 
 /${joinCommand.name} <ethereum-address>
 
